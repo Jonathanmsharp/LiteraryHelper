b2cf890875574ff40c952600a8dd2037ea6e61f8
Jonathanmsharp <jonathanmsharp@users.noreply.github.com>
2025-06-11 00:35:53 +0000

    chore: stop tracking .next/.turbo folders

--- apps/frontend/.next/static/A9R_1Ss7H11qXkt2d39Yb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-19b2fb38a021a0b1.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
